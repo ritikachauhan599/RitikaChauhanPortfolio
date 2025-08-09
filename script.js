9c6b7fe8ff8d59a0efdaa978d0202463579988ed
ritikachauhan599 <chauhanritikachauhan.58@gmail.com>
2025-08-10 05:24:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js";
+        var typed = new Typed('#element', {
+            strings: ['Frontend Developer'],
+            typeSpeed: 50,
+        });
